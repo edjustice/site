884c9a1ee7d89f032db066a43a0089e9922f9849
Aakash <aakashsingh1999@gmail.com>
2018-07-01 16:04:58 +0530

    Add files via upload

---
+++ js/about.js
@@ -0,0 +1,43 @@
+(function($) {
+    $(function() {
+        var $window = $(window),
+            muted = false,
+            $sectionHomeHeading = $('#section-home-heading');
+
+        if (!Modernizr.touch) {
+
+            /* Video background
+             *******************************************/
+            $('.video-bg').videobackground({
+                videoSource: [
+                    ['videos/home-bg.mp4', 'video/mp4']
+                ],
+                loop: true,
+                resize: false,
+                preload: true,
+                autoplay: true,
+                controls: false,
+                loadedCallback: function() {
+                    if (!muted) {
+                        muted = true;
+                        $(this).videobackground('mute');
+                    }
+                }
+            });
+
+            $('.video-bg').prop('muted', true);
+        }
+
+        /* Adjust position of programs images
+         *******************************************/
+        $window.on('resize.home', function() {
+            var $images = $('.bg img'),
+                offset = parseInt($('.bg').outerWidth() - $images.outerWidth()) / 2,
+                left = Math.min(offset, 0);
+
+            $images.css({
+                'left': left
+            })
+        }).trigger('resize.home');
+    });
+})(jQuery);
